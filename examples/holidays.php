83b703a9e8f1ca61b2204bb4fb057525995db78b
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-10-08 22:07:58 +0200

    feat: add holiday example and remove supply chain class

--- examples/holidays.php
+++ examples/holidays.php
@@ -5,14 +5,14 @@ require(__DIR__.'/../vendor/autoload.php');
 use Carbon\CarbonImmutable;
 use Carbon\CarbonInterface;
 use Fmeccanici\Dlvr\Builders\WorkScheduleBuilder;
-use Fmeccanici\Dlvr\SupplyChain;
 
 $workScheduleBuilder = new WorkScheduleBuilder();
-$workScheduleBuilder->addRegularWorkWeek();
+$workSchedule = $workScheduleBuilder->addRegularWorkWeek()->build();
+$workSchedule->addHoliday(CarbonImmutable::now()->nextWeekday());
 
 $leadTimeInWorkDays = 6;
 $now = CarbonImmutable::now()->next(CarbonInterface::MONDAY)->setHour(10)->setMinute(0);
 $dueDate = $workSchedule->dueDate($now, 6);
 
-// Due date nest Tuesday
+// Due date next Wednesday
 var_dump($dueDate->toDateTimeString());
--- examples/regular_work_schedule.php
+++ examples/regular_work_schedule.php
@@ -5,9 +5,10 @@ require(__DIR__.'/../vendor/autoload.php');
 use Carbon\CarbonImmutable;
 use Carbon\CarbonInterface;
 use Fmeccanici\Dlvr\Builders\WorkScheduleBuilder;
-use Fmeccanici\Dlvr\SupplyChain;
 
-$workSchedule = SupplyChain::createRegularWorkSchedule(new WorkScheduleBuilder());
+$workScheduleBuilder = new WorkScheduleBuilder();
+$workSchedule = $workScheduleBuilder->addRegularWorkWeek()->build();
+
 $leadTimeInWorkDays = 6;
 $now = CarbonImmutable::now()->next(CarbonInterface::MONDAY)->setHour(10)->setMinute(0);
 $dueDate = $workSchedule->dueDate($now, 6);

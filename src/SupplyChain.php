24cd9553347525e333838e8e78b500058cab316a
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-10-08 20:58:32 +0200

    feat: use SupplyChain class

---
+++ src/SupplyChain.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Fmeccanici\Dlvr;
+
+class SupplyChain
+{
+    public static function createRegularWorkSchedule(WorkScheduleBuilderInterface $workScheduleBuilder): WorkSchedule
+    {
+        $workScheduleBuilder->addRegularWorkDay(DayOfWeek::MONDAY, 9, 0, 17, 0);
+        $workScheduleBuilder->addRegularWorkDay(DayOfWeek::TUESDAY, 9, 0, 17, 0);
+        $workScheduleBuilder->addRegularWorkDay(DayOfWeek::WEDNESDAY, 9, 0, 17, 0);
+        $workScheduleBuilder->addRegularWorkDay(DayOfWeek::THURSDAY, 9, 0, 17, 0);
+        $workScheduleBuilder->addRegularWorkDay(DayOfWeek::FRIDAY, 9, 0, 17, 0);
+        $workScheduleBuilder->addRegularWorkDay(DayOfWeek::SATURDAY, 9, 0, 17, 0);
+
+        return $workScheduleBuilder->build();
+    }
+}
